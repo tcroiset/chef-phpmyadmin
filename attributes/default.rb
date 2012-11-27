fbe44a52b8fdeba210c6fca0c6137951fd417229
Anusch Athari <anusch@athari.de>
2012-11-27 02:01:46 +0100

    Added support for choosing which webserver to configure. Now defaults to apache2, with options for nginx or none. Added a more verbose README. With licensing information.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,9 +9,13 @@ default[:phpmyadmin][:cfg][:control_database]       =   'phpmyadmin'
 default[:phpmyadmin][:cfg][:control_user]           =   'phpmyadmin'
 #default[:phpmyadmin][:cfg][:control_user_password] =   '' - will be set to an automatically generated password unless specified
 
+default[:phpmyadmin][:webserver]                    =   'apache2'
+
 default[:phpmyadmin][:nginx][:port]                 =   80
 default[:phpmyadmin][:nginx][:server_name]          =   'phpmyadmin.yourhost.com'
 default[:phpmyadmin][:nginx][:docroot]              =   default[:phpmyadmin][:cfg][:path]
 default[:phpmyadmin][:nginx][:fastcgi_server]       =   'unix:/dev/shm/php5-fpm.sock'
+
+default[:phpmyadmin][:apache2][:site_config]        =   '/etc/phpmyadmin/apache.conf'
 
 
