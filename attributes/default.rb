088063f6e4efa79c7dbe4e36a10bb239e59f2fe5
Jorge Acosta Goszczyñski <jacosta@tacitknowledge.com>
2013-09-17 12:45:10 -0500

    Update default attributes adding compatibility for redhat based distros
    The paths and directory names are different for some distros

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,14 @@
-default[:phpmyadmin][:cfg][:cfg_path]               =   '/etc/phpmyadmin'
-default[:phpmyadmin][:cfg][:path]                   =   '/usr/share/phpmyadmin'
+
+case platform
+when "redhat", "centos", "scientific", "fedora"
+  default[:phpmyadmin][:cfg][:cfg_path]               =   '/etc/phpMyAdmin'
+  default[:phpmyadmin][:cfg][:path]                   =   '/usr/share/phpMyAdmin'
+  default[:phpmyadmin][:apache2][:site_config]        =   '/etc/httpd/conf.d/phpMyAdmin.conf'
+else
+  default[:phpmyadmin][:cfg][:cfg_path]               =   '/etc/phpmyadmin'
+  default[:phpmyadmin][:cfg][:path]                   =   '/usr/share/phpmyadmin'
+  default[:phpmyadmin][:apache2][:site_config]        =   '/etc/phpmyadmin/apache.conf'
+end
 
 default[:phpmyadmin][:cfg][:auth_type]              =   'cookie'
 default[:phpmyadmin][:cfg][:allow_root]             =   false
@@ -16,6 +25,6 @@ default[:phpmyadmin][:nginx][:server_name]          =   'phpmyadmin.yourhost.com
 default[:phpmyadmin][:nginx][:docroot]              =   default[:phpmyadmin][:cfg][:path]
 default[:phpmyadmin][:nginx][:fastcgi_server]       =   'unix:/dev/shm/php5-fpm.sock'
 
-default[:phpmyadmin][:apache2][:site_config]        =   '/etc/phpmyadmin/apache.conf'
+
 
 
